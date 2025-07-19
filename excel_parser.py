fae1bbb85c9e1133594dce20b5c3901f9c721488
Vinay <121495948+vinay-0821@users.noreply.github.com>
2025-07-19 11:19:49 +0530

    Update excel_parser.py

--- excel_parser.py
+++ excel_parser.py
@@ -1,7 +1,14 @@
 import pandas as pd
 
 def normalize_column_names(df):
-    df.columns = [col.strip().lower().replace(" ", "_") for col in df.columns]
+    df.columns = [
+        col.strip()
+           .lower()
+           .replace(" ", "_")
+           .replace(":", "_")
+           .replace("-", "_")
+        for col in df.columns
+    ]
     return df
 
 def process_excel(uploaded_file):
